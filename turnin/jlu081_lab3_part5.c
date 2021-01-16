2d1807706ae48a504635202fd5d0d03fb030609e
Jess-say <lu.jessie1714@gmail.com>
2021-01-16 00:25:52 -0800

    Completed part 5

---
+++ turnin/jlu081_lab3_part5.c
@@ -0,0 +1,54 @@
+/*	Author: Jessie Lu
+ *  Partner(s) Name: 
+ *	Lab Section: 23
+ *	Assignment: Lab #3  Exercise #5
+ *	Exercise Description: Airbag enabler depends on weight sensor. 
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+	DDRD = 0x00; PORTD = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+
+	unsigned char D = 0x00;
+	unsigned char B1 = 0x00;
+	unsigned char B2 = 0x00;
+	
+    	while (1) {
+		
+		D = PIND & 0xFF;
+
+		// if weight > 70 then PB1 is 1
+		// if weight < 5 B1 and B2 should be set
+		if (D >= 0x05) {
+			if (D >= 0x46) {
+				B1 = 0x02;
+			}
+			else {
+				B1 = 0x00;
+			}
+
+			// if 5 < weight < 70 then PB2 is 1
+			if (D > 0x05 && D < 0x46) {
+				B2 = 0x04;
+			}
+			else {
+				B2 = 0x00;
+			}
+		}
+		else {
+			B1 = 0x00;
+			B2 = 0x00;
+		}
+		
+
+		PORTB = B2 | B1;
+    	}
+    	return 0;
+}
